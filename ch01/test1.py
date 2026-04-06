89442a929174adfc2a2fffe1d1fc030840bfde4f
soli-robot <abcdsjj7378@gamil.com>
2026-04-06 16:41:43 +0900

    modified:   ch01/test1.py

--- ch01/test1.py
+++ ch01/test1.py
@@ -5,5 +5,4 @@ print('Hello World')
 # git push -u origin main
 
 # 혹은 그냥 메인 파일 새로 만들기로 branch 하면 된다.
-
-print("How are you?")
+# 충돌 일어날 때 덮어 쓸거냐 git config pull.rebase true, 말거냐 git config pull.rebase false 터미널에 입력하기
